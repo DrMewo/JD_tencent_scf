fce26d6b4c3b7b6b07510a10d7e374bf9de9966d
Mewo <36326138+DrMewo@users.noreply.github.com>
2021-09-08 00:36:04 +0800

    Update jd_fcdyj.js

--- jd_fcdyj.js
+++ jd_fcdyj.js
@@ -1,29 +1,3 @@
-/*
-活动入口： 京东极速版-我的-发财大赢家
- * /
- * 基于温某人大佬的脚本修改
- * 助力逻辑：优先助力互助码环境变量，中午10点之后再给我助力
- * TG交流群：https://t.me/jd_zero205
- * TG通知频道：https://t.me/jd_zero205_tz
- * /
-https://raw.githubusercontent.com/Wenmoux/scripts/master/jd/jd_fcdyj.js
-已支持IOS双京东账号, Node.js支持N个京东账号
-脚本兼容: QuantumultX, Surge, Loon, 小火箭，JSBox, Node.js
-============Quantumultx===============
-[task_local]
-#发财大赢家
-1 6-22/3 * * * https://raw.githubusercontent.com/Wenmoux/scripts/master/jd/jd_fcdyj.js, tag=新潮品牌狂欢, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-
-================Loon==============
-[Script]
-cron "1 6-22/3 * * *" script-path=https://raw.githubusercontent.com/Wenmoux/scripts/master/jd/jd_fcdyj.js tag=翻翻乐
-
-===============Surge=================
-发财大赢家 = type=cron,cronexp="1 6-22/3 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Wenmoux/scripts/master/jd/jd_fcdyj.js
-
-============小火箭=========
-发财大赢家 = type=cron,script-path=https://raw.githubusercontent.com/Wenmoux/scripts/master/jd/jd_fcdyj.js, cronexpr="1 6-22/3 * * *", timeout=3600, enable=true
- */
 const $ = new Env('发财大赢家助力');
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
